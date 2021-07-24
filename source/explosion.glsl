c14dd6039d2a58f764070a0d1346baeb6331ca4a
Paul Craven <paul@cravenfamily.com>
2021-07-24 17:20:31 -0500

    Update

--- source/explosion.glsl
+++ source/explosion.glsl
@@ -1,35 +1,41 @@
-uniform vec2 pos;
+// Thanks toi The Art of Code
+// https://www.youtube.com/watch?v=xDxAnguEOn8
 
-vec2 Hash12(float t) {
-  float x = fract(sin(t * 674.3)*453.2);
-  float y = fract(sin((t+x) * 724.3)*341.2);
-  return vec2(x, y);
-}
+uniform vec2 explosionPos;
+
+const float TWOPI = 6.2832;
+const float PARTICLE_COUNT = 125.0;
+
+// Function to return two pseudo radom numbers given an input number
+// Result is in polar coordinates to make circular rather than square
+// splat.
 vec2 Hash12_Polar(float t) {
-  float a = fract(sin(t * 674.3)*453.2)*6.2832;
-  float d = fract(sin((t+a) * 724.3)*341.2);
+  float angle = fract(sin(t * 674.3) * 453.2) * TWOPI;
+  float distance = fract(sin((t + angle) * 724.3) * 341.2);
 
-  return vec2(sin(a), cos(a)) * d;
+  return vec2(sin(angle), cos(angle)) * distance;
 }
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     // Normalized pixel coordinates (from 0 to 1)
-    vec2 npos = (pos-.5*iResolution.xy)/iResolution.y;
-    vec2 uv = (fragCoord-.5*iResolution.xy)/iResolution.y;
-//    vec2 uv = fragCoord/iResolution.xy;
+    vec2 npos = (explosionPos - .5 * iResolution.xy) / iResolution.y;
+    vec2 uv = (fragCoord- .5 * iResolution.xy) / iResolution.y;
+
+    // Move things so the explosion is at the specified coordinates
     uv -= npos;
+
     float col = 0.;
     vec3 baseColor = vec3(1., 0., 0.);
 
     float t = fract(iTime);
 
-    for (float i=0.;i<125.;i++) {
-        vec2 dir = Hash12_Polar(i+1.0);
+    for (float i= 0.; i < PARTICLE_COUNT; i++) {
+        vec2 dir = Hash12_Polar(i + 1.0);
 
         float d = length(uv - dir * t);
         float brightness = mix(.0005, .002, smoothstep(.1, 0., t));
 
-        brightness *= sin(t*20.+i)*.5+.5;
+        brightness *= sin(t * 20. + i) * .5 + .5;
         col += brightness/d;
     }
     // Output to screen
--- source/explosion.py
+++ source/explosion.py
@@ -4,7 +4,7 @@ from arcade.experimental.shadertoy import Shadertoy
 class ExplosionMaker:
     def __init__(self, size, position):
         self.shadertoy: Shadertoy = Shadertoy.create_from_file(size, "explosion.glsl")
-        self.shadertoy.program['pos'] = position
+        self.shadertoy.program['explosionPos'] = position
         self.time = 0.0
         self.position = position
 
